28251f76b30437fb36ad68d50a185f4aa73708bc
bolyn1 <deremin06@gmail.com>
2025-05-12 18:14:31 +0300

    Remove seed_challenges.py

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-from .auth import AuthManager
-from .database import DBManager, seed_challenges
-from .game import GameWindow
-from .ui import ChallengesWindow, LeaderboardWindow, LoginAuthWindow
--- src/database/__init__.py
+++
@@ -1,2 +0,0 @@
-from .db_manager import DBManager
-from .seed_challenges import seed_challenges
